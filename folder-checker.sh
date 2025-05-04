4542f62286e82bebe0f4d5db03f94106ecdb4733
ido <idoyana.work@gmail.com>
2025-05-04 17:32:37 +0000

    הוספתי סקריפט שמחפש תיקיות ויוצר אותן אם לא קיימות

---
+++ folder-checker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "enter the folder name:"
+read folder_name
+
+if [ -d "$folder_name" ]; then
+    echo "the folder '$folder_name' is already exists"
+else
+
+   mkdir "$folder_name"
+   echo "the folder '$folder_name' have been crated successfully"
+fi
