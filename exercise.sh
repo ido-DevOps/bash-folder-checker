23ce30b90550bfc5fd9a7224bdcd281a0795178c
ido <idoyana.work@gmail.com>
2025-05-05 15:17:53 +0000

    Add exercise.sh: prints sustem info

---
+++ exercise.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# סקריפט שמדפיס:
+# 1. שם המערכת (hostname)
+# 2. תאריך ושעה נוכחיים
+# 3. רשימת קבצים בתיקייה הנוכחית
+
+echo "Hostname: $(hostname)"
+echo "Date:     $(date)"
+echo "Files in $(pwd):"
+ls -lah
+
+
+
